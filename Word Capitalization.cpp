f135bad1fa245138d2c0729c8ee786d3dec371e8
Manavesh Narendra <53822509+manu-karenite@users.noreply.github.com>
2021-02-09 00:11:26 +0530

    281A In C++ Language

---
+++ Word Capitalization.cpp
@@ -0,0 +1,25 @@
+/*
+This code is written by Manavesh Narendra
+E-mail : manu.karenite@gmail.com
+LinkedIn : https://www.linkedin.com/in/manavesh-narendra-489833196/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ char str[1001];
+ cin>>str;
+ 
+ char c=str[0];
+ if((int)c>=97)
+    c=(char)((int)c-32);
+str[0]=c;
+ 
+cout<<str;
+ 
+ 
+ 
+ 
+}
