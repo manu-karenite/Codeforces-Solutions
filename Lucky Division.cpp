d67b5c30f53372553505914a9ce4d01ee28356bc
Manavesh Narendra <53822509+manu-karenite@users.noreply.github.com>
2021-02-09 00:27:07 +0530

    122A in C++ Language

---
+++ Lucky Division.cpp
@@ -0,0 +1,26 @@
+/*
+This code is written by Manavesh Narendra
+E-mail : manu.karenite@gmail.com
+LinkedIn : https://www.linkedin.com/in/manavesh-narendra-489833196/
+*/
+
+
+#include<iostream>
+using namespace std;
+ 
+ 
+int main()
+ 
+{
+   int n;
+ 
+   cin>>n;
+ 
+   if(n%47==0||n%74==0||n%744==0||n%474==0||n%4==0||n%7==0||n%77==0||n%44==0||n%444==0||n%777==0||n%447==0||n%477==0||n%747==0||n%774==0)
+   {
+       cout<<"YES"<<endl;
+   }
+   else{
+       cout<<"NO"<<endl;
+   }
+}
