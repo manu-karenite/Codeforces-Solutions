01d24d16c5641eb604800f1667b83ea76dc10a0e
Manavesh Narendra <53822509+manu-karenite@users.noreply.github.com>
2021-03-29 02:17:04 +0530

    1369A in C++ Language

---
+++ FashionabLee.cpp
@@ -0,0 +1,25 @@
+/*
+This code is written by Manavesh Narendra
+E-mail : manu.karenite@gmail.com
+LinkedIn : https://www.linkedin.com/in/manavesh-narendra-489833196/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int test;
+    cin>>test;
+    int entry;
+    while(test--)
+    {
+        cin>>entry;
+        if(entry%4!=0)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+
+    }
+    return 0;
+}
