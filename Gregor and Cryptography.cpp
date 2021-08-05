6854dec34b911bc01c8239754433eb9f343dfb7d
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-08-05 13:19:25 +0530

    1549A

---
+++ Gregor and Cryptography.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int a = n - 1;
+    int b = a / 2;
+    cout << b << " " << a << endl;
+  }
+  return 0;
+
+}
